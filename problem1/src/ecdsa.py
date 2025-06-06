a7d377abe5c742eb2d91e5bd2ceff8bf3412594c
Marcos <marcospantoja21@outlook.com>
2025-06-06 16:32:33 -0400

    fixed key

--- problem1/src/ecdsa.py
+++ problem1/src/ecdsa.py
@@ -1,14 +1,14 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 
-def ecdsa_signature(data, private_key):
+def sign(data, private_key):
     signature = private_key.sign(data, ec.ECDSA(hashes.SHA256()))
     return signature
 
-def ecdsa_verify(signature, data, public_key):
+def verify(signature, data, public_key):
     public_key.verify(signature, data, ec.ECDSA(hashes.SHA256))
 
-def ecdsa_generate_key():
+def generate_keys():
     private_key = ec.generate_private_key(ec.SECP521R1())
     public_key = private_key.public_key()
     return private_key, public_key
