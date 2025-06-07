64e08db4692c45023ef480f458ac8c75ebe17df8
Marcos <marcospantoja21@outlook.com>
2025-06-06 22:18:13 -0400

    1

--- problem1/src/aes.py
+++ problem1/src/aes.py
@@ -5,13 +5,15 @@ import os
 # encrpyts using AES in CBC mode 
 def encrypt(plain_text, aes_key):
     iv =  b'\x00' * 16 
-    encryptor = Cipher(algorithms.AES(aes_key), modes.CBC(iv))
+    cipher = Cipher(algorithms.AES(aes_key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
     cipher_text = encryptor.update(plain_text) + encryptor.finalize()
     return cipher_text
 
 # receiver decrypts using the senders ciphertext iv and shared key
 def decrypt(cipher_text, aes_key):
     iv =  b'\x00' * 16 
-    decrypter = Cipher(algorithms.AES(aes_key), modes.CBC(iv))
+    cipher = Cipher(algorithms.AES(aes_key), modes.CBC(iv))
+    decrypter = cipher.decryptor()
     plaintext = decrypter.update(cipher_text) + decrypter.finalize()
     return plaintext
