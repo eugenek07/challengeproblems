71f1868358a1faf8461cdff98c1c0ce824d17548
Marcos <marcospantoja21@outlook.com>
2025-07-24 23:40:36 -0400

    merges

---
+++ problem2/sender.py
@@ -0,0 +1,51 @@
+import socket
+from scapy.all import * # used to create fake but realistic packets to send to the receiver
+import random
+
+def send_fake_packets(data, host, dest, port): 
+    '''
+        This function will create fake HTTP packets (i.e., TCP packets with HTTP data) and will send the 
+        packet to the receiver. This packet has two special features: 
+        --> "data offset" attribute contains a value: 00, 01, 10, or 11. This value is the byte number
+        in the "sequence number" attribute of the TCP header that will contain our data
+        --> One of four bytes in the "sequence number" contains data that we are sending to the receiver 
+    '''
+
+    IP_layer = IP(src = host, dst = dest) # starting our packet by modifying the IP layer to have a destination
+    TCP_layer = TCP(sport = RandShort(), dport = int(port), flags = "PA") # modify TCP layer to make the packet HTTP (port 80)
+                                                                    # and specifically a realistic SYN packet  
+                                                                    # (flag PA for push-ack)
+    final_packet = IP_layer / TCP_layer    
+    send(final_packet)
+
+# def send_data(data, host, port):
+#     """
+#     This function sends data from a buffer over TCP to the specified host and port.
+#     :param data: The data to be sent.
+#     :param host: The host to send the data to.
+#     :param port: The port to send the data to.
+#     """
+#     # Create a TCP/IP socket
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#     # Connect the socket to the host and port
+#     server_address = (host, int(port))
+#     sock.connect(server_address)
+
+#     try:
+#         # Send data
+#         # encode() converts string to bytes to send over the wire
+#         sock.sendall(data.encode('utf-8'))
+#         print("Data sent: " + data)
+#     finally:
+#         # Close the socket
+#         sock.close()
+
+def main():
+    # host variable is hardcoded to localhost (127.0.0.1), port is hardcoded to 8080
+    # data is set to the single character "A"
+    # Try changing these values or even allowing the user to input them at runtime or on the command line
+    # send_data("A", "127.0.0.1", "8080")
+    send_fake_packets("A", "192.168.13.4", "192.168.12.250", "8080")
+
+main()
